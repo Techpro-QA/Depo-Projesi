60185f87e82e5ddfba198cac5676ca6c44ded10a
kircahavva <havva101997@gmail.com>
2025-06-07 22:51:21 +0300

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,96 @@
+package depo_project;
+
+import java.time.LocalDate;
+
+public class Product {
+
+    private String id;
+    private String name;
+    private String description;
+    private String category;
+    private int quantity;
+    private double price;
+    private String supplier;
+    private LocalDate expirationDate;
+
+    // Constructor
+    public Product(String id, String name, String description, String category,
+                   int quantity, double price, String supplier, LocalDate expirationDate) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.quantity = quantity;
+        this.price = price;
+        this.supplier = supplier;
+        this.expirationDate = expirationDate;
+    }
+
+     //Getter Setter
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+
+}
